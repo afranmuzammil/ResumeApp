33440eb06bd364883f8a8d6578e0dfb7d0cbfd04
shaik muzammil <52369523+afranmuzammil@users.noreply.github.com>
2021-12-23 17:50:56 +0530

    File download and open from firebase

--- lib/pages/Detalis.dart
+++ lib/pages/Detalis.dart
@@ -1,6 +1,11 @@
+//import 'dart:html';
+import 'dart:io';
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Details extends StatefulWidget {
@@ -19,6 +24,10 @@ class _DetailsState extends State<Details> {
       print('could not launch $command');
     }
   }
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -95,10 +104,58 @@ class _DetailsState extends State<Details> {
                       const TextStyle(fontSize: 16, fontWeight: FontWeight.w500))),
               const SizedBox(height: 5.0),
               //resume
+              ElevatedButton(
+                onPressed: () async{
+                  print("pressed");
+                  await openFile(
+                  url:'https://firebasestorage.googleapis.com/v0/b/resume-app-36441.appspot.com/o/files%2Ftutorial.pdf?alt=media&token=7151cf8b-9953-4ba7-8363-484285884f61',
+                  fileName:'tutorial.pdf',
+                );
+                },
+                  child:const Text("Download & Open"),
+              )
             ],
           )
         ) ,
       ),
     );
   }
+
+  Future openFile({required String url, String? fileName}) async {
+    final file = await downloadFile(url,fileName!);
+    print("from fille ${file==null}");
+    if(file==null) return;
+    print('path:${file.path}');
+    OpenFile.open(file.path);
+  }
+  Future<File?> downloadFile(String url, String name)async {
+
+    final appStorage = await getApplicationDocumentsDirectory();
+    final file = File('${appStorage.path}/$name');
+
+
+    try {
+      final response = await Dio().get(
+        url,
+        options: Options(
+          responseType: ResponseType.bytes,
+          followRedirects: false,
+          receiveTimeout: 0,
+        ),
+      );
+     // print("from fille $url");
+      final raf = file.openSync(mode: FileMode.write);
+      print("hello : ${file == null}");
+      raf.writeFromSync(response.data);
+      await raf.close();
+
+      return file;
+    }catch(e){
+      print(" in file down $e");
+      return null;
+    }
+  }
+
+
+
 }
